d682c600d3fa92539b1c4f1b36e73b6763bb0f1f
DipuBarua <you@example.com>
2023-10-05 14:48:28 +0600

    left,right side navbar updated

--- src/pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,8 +1,26 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const LeftSideNav = () => {
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        fetch('categories.json')
+            .then(res => res.json())
+            .then(data => setCategories(data))
+    }, [])
+
     return (
-        <div>
-            <p className=" text-2xl font-poppins">THis is Left side</p>
+        <div className=" border border-black">
+            <h2 className=" text-2xl font-bold">All Caterogy</h2>
+            {
+                categories.map(category =>
+                    <Link
+                        className=" block space-y-2 ml-2 font-semibold text-xl"
+                        key={category.id}>
+                        {category.name}
+                    </Link>)
+            }
         </div>
     );
 };
--- src/pages/Shared/RightSideNav/RightSideNav.jsx
+++ src/pages/Shared/RightSideNav/RightSideNav.jsx
@@ -1,9 +1,53 @@
-
+import { BsFacebook, BsGithub, BsGoogle, BsInstagram, BsTwitter } from 'react-icons/bs';
+import qZone1 from '../../../assets/qZone1.png';
+import qZone2 from '../../../assets/qZone2.png';
+import qZone3 from '../../../assets/qZone3.png';
 
 const RightSideNav = () => {
     return (
-        <div>
-            <p className=" text-2xl"> right side</p>
+        <div className=' border border-black'>
+
+            <div className=' p-4 space-y-2 mb-5'>
+                <h2 className=' text-2xl font-poppins font-semibold mb-2'>Login With</h2>
+                <button className=' btn btn-outline w-full'>
+                    <BsGoogle></BsGoogle>
+                    Login With Google
+                </button>
+                <button className=' btn btn-outline w-full'>
+                    <BsGithub></BsGithub>
+                    Login With Github
+                </button>
+            </div>
+
+            <div className=' p-4 mb-5'>
+                <h2 className=' text-2xl font-poppins font-semibold mb-2'>Find Us On</h2>
+                <a className=' flex items-center p-4 text-lg border border-black rounded-t-lg' href="">
+                    <div className=' mr-2 border rounded-full p-3 text-blue-700 bg-gray-200'>
+                        <BsFacebook></BsFacebook>
+                    </div>
+                    Facebook
+                </a>
+                <a className=' flex items-center p-4 text-lg border-x border-black ' href="">
+                    <div className=' mr-2 border rounded-full p-3 text-blue-500 bg-gray-200'>
+                        <BsTwitter></BsTwitter>
+                    </div>
+                    Twitter
+                </a>
+                <a className=' flex items-center p-4 text-lg border border-black rounded-b-lg' href="">
+                    <div className=' mr-2 border rounded-full p-3 text-pink-600 bg-gray-200'>
+                        <BsInstagram></BsInstagram>
+                    </div>
+                    Instagram
+                </a>
+            </div>
+
+            <div className=' p-4 space-y-2 mb-5'>
+                <h2 className=' mb-2 text-2xl font-poppins font-semibold'>Q-Zone</h2>
+                <img src={qZone1} alt="" />
+                <img src={qZone2} alt="" />
+                <img src={qZone3} alt="" />
+            </div>
+
         </div>
     );
 };
